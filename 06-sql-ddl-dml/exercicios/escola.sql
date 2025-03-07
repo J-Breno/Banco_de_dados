cef4adba457d04c2230b957c28bfe457e442b7ae
J-Breno <joao.breno85@hotmail.com>
2025-03-07 16:24:55 -0300

    Fazendo as INSERÇÕES do exercicio

--- 06-sql-ddl-dml/exercicios/escola.sql
+++ 06-sql-ddl-dml/exercicios/escola.sql
@@ -49,3 +49,21 @@ CREATE TABLE tb_resultado(
 	FOREIGN KEY(avaliacao_id) REFERENCES tb_avaliacao(id)
 );
 
+INSERT INTO tb_curso(id, nome, carga_horaria, valor, nota_prevista, nota_minima) VALUES(1, 'HTML Básico', 10, 80.0, 100.0, 70.0);
+
+INSERT INTO tb_turma(id, numero, inicio, vagas, curso_id) VALUES(1, 1, '2017-09-10', 30, 1);
+
+INSERT INTO tb_aluno(cpf, nome, nascimento) VALUES('123456789-11', 'Carlos Silva', '1990-07-21');
+INSERT INTO tb_aluno(cpf, nome, nascimento) VALUES('987654321-11', 'Maria Clara', '1991-09-03');
+
+
+INSERT INTO tb_matricula(turma_id, aluno_id, data, prestacoes) VALUES(1, '123456789-11', '2017-09-05', 6);
+INSERT INTO tb_matricula(turma_id, aluno_id, data, prestacoes) VALUES(1, '987654321-11', '2017-09-06', 12);
+
+INSERT INTO tb_avaliacao(id, nota, data, turma_id) VALUES(1, 40.0, '2017-10-20', 1);
+INSERT INTO tb_avaliacao(id, nota, data, turma_id) VALUES(2, 60.0, '2017-11-30', 1);
+
+INSERT INTO tb_resultado(aluno_id, avaliacao_id, nota_obtida) VALUES('123456789-11', 1, 35.0);
+INSERT INTO tb_resultado(aluno_id, avaliacao_id, nota_obtida) VALUES('987654321-11', 1, 36.5);
+INSERT INTO tb_resultado(aluno_id, avaliacao_id, nota_obtida) VALUES('123456789-11', 2, 47.0);
+INSERT INTO tb_resultado(aluno_id, avaliacao_id, nota_obtida) VALUES('987654321-11', 2, 52.4);
