5b4fc12d4ea7ed2f9eff80089bf68d2e4f435a2f
J-Breno <joao.breno85@hotmail.com>
2025-03-07 01:47:33 -0300

    renomeando nome da coluna e fazendo inserções

--- 06-sql-ddl-dml/exercicios/rede_social.sql
+++ 06-sql-ddl-dml/exercicios/rede_social.sql
@@ -46,3 +46,27 @@ CREATE TABLE tb_foto(
 );
 
 ALTER TABLE tb_usuario ADD FOREIGN KEY(foto_de_perfil_id) REFERENCES tb_foto(uri);
+
+ALTER TABLE tb_usuario RENAME COLUMN gereno TO genero;
+
+INSERT INTO tb_usuario(id, nome, email, nascimento, website, genero, telefone, foto_de_perfil_id) 
+VALUES(1, 'João Silva', 'joão@gmail.com', '1991-10-15', null, 'M', null, null);
+INSERT INTO tb_usuario(id, nome, email, nascimento, website, genero, telefone, foto_de_perfil_id) 
+VALUES(2, 'Maria Alice', 'alice@gmail.com', '1999-04-15', 'youtube.com/alice', 'F', null, null);
+INSERT INTO tb_usuario(id, nome, email, nascimento, website, genero, telefone, foto_de_perfil_id) 
+VALUES(3, 'Carlos Alves', 'carlos@yahoo.com', null, null, 'M', '9777-8383', null);
+INSERT INTO tb_usuario(id, nome, email, nascimento, website, genero, telefone, foto_de_perfil_id) 
+VALUES(4, 'Ana Clara', 'clara@gmail.com', '1997-01-23', 'blog.clara.com', 'M', '8181-2820', null);
+
+INSERT INTO tb_seguidores(seguidor_id, seguido_id) VALUES(1, 3);
+INSERT INTO tb_seguidores(seguidor_id, seguido_id) VALUES(1, 4);
+INSERT INTO tb_seguidores(seguidor_id, seguido_id) VALUES(3, 4);
+INSERT INTO tb_seguidores(seguidor_id, seguido_id) VALUES(2, 4);
+
+INSERT INTO tb_postagem(id, texto, instante, autor_id) VALUES(1, '#partiu festa!', '2017-08-09 19:34:20', 4);
+INSERT INTO tb_postagem(id, texto, instante, autor_id) VALUES(2, 'Bom dia pessoal!', '2017-08-10 07:50:00', 4);
+
+INSERT INTO tb_album(id, titulo, instante_de_postagem, usuario_id) VALUES(1, 'Férias de verão', '2017-01-13 13:50:13', 3);
+
+INSERT INTO tb_foto(uri, instante_de_postagem, postagem_id, usuario_id, album_id) VALUES('http://dominio.com/praia1.jpg', '2017-01-10 10:30:14', null, 3, 1);
+INSERT INTO tb_foto(uri, instante_de_postagem, postagem_id, usuario_id, album_id) VALUES('http://dominio.com/praia2.jpg', '2017-01-10 10:31:12', null, 3, 1);
