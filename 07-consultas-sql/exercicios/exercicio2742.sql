9b82f6329604df26269afb00dba6dade875e2b2c
J-Breno <joao.breno85@hotmail.com>
2025-03-25 08:54:46 -0300

    exercicio

--- 07-consultas-sql/exercicios/exercicio2742.sql
+++ 07-consultas-sql/exercicios/exercicio2742.sql
@@ -1,28 +1,4 @@
-CREATE TABLE dimensions(
-	id INTEGER PRIMARY KEY,
-	name varchar(255)
-);
-
-CREATE TABLE life_registry(
-	id INTEGER PRIMARY KEY,
-	name VARCHAR(255),
-	omega NUMERIC,
-	dimensions_id INTEGER REFERENCES dimensions (id)
-);
-
-
-INSERT INTO dimensions(id, name)
-VALUES 
-      (1, 'C774'),
-      (2, 'C784'),
-      (3, 'C794'),
-      (4, 'C824'),
-      (5, 'C875');
-      
-INSERT INTO life_registry(id, name, omega, dimensions_id)
-VALUES
-	  (1, 'Richard Postman', 5.6, 2),	
-	  (2, 'Simple Jelly', 1.4, 1),	
-	  (3, 'Richard Gran Master', 2.5, 1),	
-	  (4, 'Richard Turing', 6.4, 4),	
-	  (5, 'Richard Strall',	1.0, 3);
+SELECT l.name, ROUND(l.omega * 1.618, 3) AS "The N Factor"
+FROM life_registry l
+INNER JOIN dimensions d ON l.dimensions_id = d.id
+WHERE l.name LIKE 'Richard%' AND (d.name = 'C875' OR d.name = 'C774')
